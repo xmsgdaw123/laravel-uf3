2b28da71d8c97ed36fc4dc28da89cc5e8d1d874c
xmsgdaw123 <m.sepulveda1dawnuria2020@gmail.com>
2022-04-23 20:18:06 +0200

    home view

---
+++ resources/views/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <div>
+        <a href="/">PostsApp</a>
+    </div>
+    <nav class="header-nav">
+        <div><a href="/">Inicio</a></div>
+        <div><a href="/login">Login</a></div>
+        <div><a href="/login">Registro</a></div>
+    </nav>
+</header>
